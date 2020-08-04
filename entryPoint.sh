fd8b517979d556ee90d0791dbb4c5a9e160b2f0f
GuyG <guyg@flytrex.com>
2020-08-04 13:35:21 +0300

    working with tcp and publishing port

--- entryPoint.sh
+++ entryPoint.sh
@@ -2,4 +2,4 @@
 echo SYSID_THISMAV=$1 | tee -a /ardupilot/Tools/autotest/default_params/copter.parm
 cat /external/extra-locations.txt >> /ardupilot/Tools/autotest/locations.txt
 START_LOCATION=$(< /external/start-location.conf)
-sim_vehicle.py -N -v ArduCopter --frame=hexa -L $START_LOCATION -w --model hexa --no-mavproxy --sitl-instance-args="-S --base-port 5760" $SIM_OPTIONS 
+sim_vehicle.py -N -v ArduCopter --frame=hexa -L $START_LOCATION -w --model hexa --no-mavproxy --sitl-instance-args="-S --base-port $GCS_PORT" $SIM_OPTIONS
--- runMAV.sh
+++ runMAV.sh
@@ -16,7 +16,9 @@ echo SYS_ID=$SYS_ID GCS_HOST=$2 GCS_PORT=$3
 echo SIM_OPTIONS=$SIM_OPTIONS
 docker run --rm -it \
   -v $SCRIPTS_DIR:/external \
-  -e "SIM_OPTIONS=--out=udpout:$GCS_HOST:$GCS_PORT --out=udpout:$GCS_HOST:14550 -m --target-system=$SYS_ID $SIM_OPTIONS" \
+  -p $GCS_HOST:$GCS_PORT:$GCS_PORT/tcp \
+  -e "GCS_PORT=$GCS_PORT" \
+  -e "SIM_OPTIONS=-m --target-system=$SYS_ID $SIM_OPTIONS" \
   --entrypoint "/external/entryPoint.sh" \
   flytrex/flyhawk-sitl-docker:latest $SYS_ID
 exit $?
