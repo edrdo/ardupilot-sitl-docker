320ef8880720cf146e6bd489b5f68dba982f4967
GuyG <guyg@flytrex.com>
2020-08-05 11:04:44 +0300

    added option to run with parameter file

--- runMAV.sh
+++ runMAV.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
 
-if [ $# -ne 3 ] 
+if [ $# -lt 3 ] || [ $# -gt 4 ] 
 then
-  echo "Usage: runMAV.sh <system_id> <gcs_host> <gcs_port>"
+  echo "Usage: runMAV.sh <system_id> <gcs_host> <gcs_port> [<optional_param_file>]"
   exit 1
 fi
 
 SYS_ID=$1
 GCS_HOST=$2
 GCS_PORT=$3
+if [ $# -eq 4 ]
+then
+  PARAM_FILE="--add-param-file=$4"
+fi
 SCRIPTS_DIR=$(cd $(dirname $(which $0)); pwd)
 
 echo "Running SITL simulation ..."
@@ -18,6 +22,7 @@ docker run --rm -it \
   -v $SCRIPTS_DIR:/external \
   -p $GCS_HOST:$GCS_PORT:$GCS_PORT/tcp \
   -e "GCS_PORT=$GCS_PORT" \
+  -e "PARAM_FILE=$PARAM_FILE" \
   -e "SIM_OPTIONS=-m --target-system=$SYS_ID $SIM_OPTIONS" \
   --entrypoint "/external/entryPoint.sh" \
   flytrex/flyhawk-sitl-docker:latest $SYS_ID
