c06ec19605759aea2382a28d6506a01be52b3bbe
GuyG <guyg@flytrex.com>
2020-09-01 10:49:06 +0300

    added scripts dir mount to cshell

--- cshell.sh
+++ cshell.sh
@@ -1,6 +1,9 @@
+SCRIPTS_DIR=$(cd $(dirname $(which $0)); pwd)
+
 echo "Docker container will now start ..."
 docker run \
   --rm -h ardupilot-sitl -it \
+  -v $SCRIPTS_DIR:/external \
   --cap-add=SYS_PTRACE \
   --security-opt seccomp=unconfined \
   -u ardupilot flytrex/flyhawk-sitl-docker:latest /bin/bash
