cfeb31506cffab5858c534bc843790c8836adbff
Kazusumi <Ogihara.iti@gmail.com>
2021-08-08 12:10:30 +0900

    base iamge version up

--- runMAV.sh
+++ runMAV.sh
@@ -13,7 +13,8 @@ fi
 SYS_ID=$1
 GCS_HOST=$2
 GCS_PORT=$3
-SCRIPTS_DIR=$(cd $(dirname $(which $0)); pwd)
+#SCRIPTS_DIR=$(cd $(dirname $(which $0)); pwd)
+SCRIPTS_DIR=$(dirname $(which $0); pwd)
 
 echo "Running SITL simulation ..."
 echo SYS_ID=$SYS_ID GCS_HOST=$2 GCS_PORT=$3
@@ -21,6 +22,8 @@ echo SIM_OPTIONS=$SIM_OPTIONS
 docker run -itd \
   -v $SCRIPTS_DIR:/external \
   -e "SIM_OPTIONS=--out=udpout:$GCS_HOST:$GCS_PORT --out=udpout:$GCS_HOST:14550 -m --target-system=$SYS_ID $SIM_OPTIONS" \
+  -p 5760:5760 -p 5501:5501/udp \
   --entrypoint "/external/entryPoint.sh" \
-  edrdo/ardupilot-sitl-docker:latest $SYS_ID
+  temmiehoihoi/sitl_image:latest $SYS_ID
 exit $?
+#  -p $GCS_PORT:$GCS_PORT/udp -p 14550:14550/udp -p 5501:5501/tcp \
